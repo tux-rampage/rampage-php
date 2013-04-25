9f74a38b46ea0ec21db9bb3e9e294bef76fc1513
tux-rampage <staff@pro-unreal.de>
2013-04-25 17:53:49 +0200

    Adding code base for very simple orm implementation.
    rampage.orm will be removed in favour of Doctrine

---
+++ library/rampage/simpleorm/EntityManager.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * This is part of rampage.php
+ * Copyright (c) 2013 Axel Helmert
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @category  library
+ * @package   rampage.simpleorm
+ * @author    Axel Helmert
+ * @copyright Copyright (c) 2013 Axel Helmert
+ * @license   http://www.gnu.org/licenses/gpl-3.0.txt GNU General Public License
+ */
+
+namespace rampage\simpleorm;
+
+use rampage\db\Adapter;
+/**
+ * Entity manager
+ */
+class EntityManager
+{
+    /**
+     * @var \rampage\db\Adapter
+     */
+    private $adapter = null;
+
+    /**
+     * @param ConfigInterface $config
+     */
+    public function __construct(ConfigInterface $config)
+    {
+        $this->adapter = new Adapter($config->getAdapterOptions());
+    }
+
+    /**
+     * @return \rampage\db\Adapter
+     */
+    public function getAdapter()
+    {
+        return $this->adapter;
+    }
+}
